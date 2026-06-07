51b3247aa29b08c8d5b7a12cb04a8c3af78ccbce
maurolibanio <maurolibanio@gmail.com>
2026-06-07 15:43:50 +0200

    Update fetch_events.py

--- src/fetch_events.py
+++ src/fetch_events.py
@@ -1,5 +1,9 @@
 import json
 import requests
+from datetime import date, timedelta
+
+today = date.today()
+future = today + timedelta(days=365)
 
 url = (
     "https://www.visitoslo.com/api/eventlist/events"
@@ -10,18 +14,24 @@ url = (
     "505772,506922,506802,519862,506032,"
     "513572,506232,505792,500602,"
     "508172,508182,502032,507222,509172"
+    f"&FromDate={today.isoformat()}"
+    f"&ToDate={future.isoformat()}"
 )
 
 response = requests.get(url)
-
 response.raise_for_status()
+
+data = response.json()
 
 with open("events.json", "w", encoding="utf-8") as f:
     json.dump(
-        response.json(),
+        data,
         f,
         ensure_ascii=False,
         indent=2
     )
 
-print("Events downloaded")
+print(
+    f"Downloaded {len(data['events'])} events "
+    f"from {today} to {future}"
+)
